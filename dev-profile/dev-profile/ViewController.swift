e3d3bfbd57dd2d51e540f334bef23b70f50d1751
adam mcbride <adam.mcbride@gmail.com>
2017-10-20 14:04:48 -0400

    built the layout in autolayout with constraints. works in iPhone, but doesn't look great on iPad.
    Important Note: took a long time to figure out that i was having serious problems with autolayout due to "Safe Areas" being enabled within the project View Controller. make sure this is disabled for the rest of the project lessons, as they do not address the issue when dealing with Xcode 9 Beta-- it wasn't introduced as a feature until a later Beta build, and introduces some new complications when dealing with the newer iPhone 8, 8+, and iPhone X that isn't covered in these lessons which can really fuck up your layouts and make the lessons go off the rails. disable the option for now.

--- dev-profile/dev-profile/ViewController.swift
+++ dev-profile/dev-profile/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var devslopesProfileLogo: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //give the profile logo rounded corners
+        devslopesProfileLogo.layer.cornerRadius = 5
+        devslopesProfileLogo.layer.masksToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
